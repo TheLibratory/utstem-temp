1573664d31d73110a8ee0d1879f731eddf19ab7f
Casey Hunt <casey.hunt@gmail.com>
2013-03-29 12:40:09 -0500

    Initial commit

---
+++ templates/utbranding.php
@@ -0,0 +1,10 @@
+<div id="utlib-branding-bar">
+	<div class="branding-bar bar-ut">
+    	<a href="http://www.utexas.edu/"><img src="<?php print $base_path . $directory; ?>/images/utlib-footer-tower.jpg"></a>
+	</div>
+    <div class="branding-bar bar-library">
+    	<div id="bar-liblogo">
+        	<a href="http://lib.utexas.edu/"><img src="<?php print $base_path . $directory; ?>/images/utlib-footer-flyingbook.gif"></a>
+		</div>
+	</div>
+</div>
