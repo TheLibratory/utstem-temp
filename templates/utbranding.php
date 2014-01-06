801acc80716372727b309c7360d16087f0bf015f
Dustin Younse <milsyobtaf@gmail.com>
2014-01-06 17:04:54 -0600

    Removed warning to put in a better location

--- templates/utbranding.php
+++ templates/utbranding.php
@@ -7,8 +7,4 @@
         	<a href="http://lib.utexas.edu/"><img src="<?php print $base_path . $directory; ?>/images/utlib-footer-flyingbook.gif"></a>
 		</div>
 	</div>
-    <div class="messages warning">
-        <p>This conference is over. This website remains as a historical archive.</p>
-        <p>For more University of Texas Libraries conferences, please check <a href="http://conferences.lib.utexas.edu">http://conferences.lib.utexas.edu</a>.</p>
-    </div>
 </div>
